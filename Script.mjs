75733e72071c9d993756532a66cfb0ef2973b9a0
Girma <93812274+206724@users.noreply.github.com>
2024-05-07 22:07:29 -0700

    second commit

--- Script.mjs
+++ Script.mjs
@@ -1,29 +1,29 @@
 //** Part 1: Fizz Buzz*/
 
-// Loop through all numbers from 1 to 100.
-// for (let i =1;i<100; i++){  
-//     if (i% 3 == 0)            
-//         {
-//             console.log("Fizz")  //If a number is divisible by 3, log “Fizz.”
+//Loop through all numbers from 1 to 100.
+for (let i =1;i<100; i++){  
+    if (i% 3 == 0)            
+        {
+            console.log("Fizz")  //If a number is divisible by 3, log “Fizz.”
        
-//         }
-//     else if (i% 5 ==0)
-//         {
-//             console.log("Buzz")   // If a number is divisible by 5, log “Buzz.
+        }
+    else if (i% 5 ==0)
+        {
+            console.log("Buzz")   // If a number is divisible by 5, log “Buzz.
           
-//         }
-//     else if ( i%3 ==0 && i% 5 ==0)
-//         {
-//             console.log("Fizz Buzz")   // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+        }
+    else if ( i%3 ==0 && i% 5 ==0)
+        {
+            console.log("Fizz Buzz")   // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
           
-//         }    
-//     else
-//     {
-//         console.log(i)                //If a number is not divisible by either 3 or 5, log the number.
-//     }
+        }    
+    else
+    {
+        console.log(i)                //If a number is not divisible by either 3 or 5, log the number.
+    }
     
     
-//  }
+ }
 
 //*************Part 2: Prime Time*********** */
 
@@ -53,4 +53,3 @@ for (let i=2;i<n;i++)
 
     //**** Part 3: Feeling Loopy***************/
 
-    
