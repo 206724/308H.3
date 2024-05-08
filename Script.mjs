a232e2895b31c823263e67219ef5a435eac865d1
Girma <93812274+206724@users.noreply.github.com>
2024-05-07 22:41:52 -0700

    my third commit

--- Script.mjs
+++ Script.mjs
@@ -1,55 +1,65 @@
 //** Part 1: Fizz Buzz*/
 
 //Loop through all numbers from 1 to 100.
-for (let i =1;i<100; i++){  
-    if (i% 3 == 0)            
-        {
-            console.log("Fizz")  //If a number is divisible by 3, log “Fizz.”
+// for (let i =1;i<100; i++){  
+//     if (i% 3 == 0)            
+//         {
+//             console.log("Fizz")  //If a number is divisible by 3, log “Fizz.”
        
-        }
-    else if (i% 5 ==0)
-        {
-            console.log("Buzz")   // If a number is divisible by 5, log “Buzz.
+//         }
+//     else if (i% 5 ==0)
+//         {
+//             console.log("Buzz")   // If a number is divisible by 5, log “Buzz.
           
-        }
-    else if ( i%3 ==0 && i% 5 ==0)
-        {
-            console.log("Fizz Buzz")   // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+//         }
+//     else if ( i%3 ==0 && i% 5 ==0)
+//         {
+//             console.log("Fizz Buzz")   // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
           
-        }    
-    else
-    {
-        console.log(i)                //If a number is not divisible by either 3 or 5, log the number.
-    }
+//         }    
+//     else
+//     {
+//         console.log(i)                //If a number is not divisible by either 3 or 5, log the number.
+//     }
     
     
- }
+//  }
 
-//*************Part 2: Prime Time*********** */
+// //*************Part 2: Prime Time*********** */
 
-let n =100;
+// let n =100;
 
-for (let i=2;i<n;i++)
-    {    
-        if( i ===2 || i===3 || i===5 || i==7)
-            {
-                console.log(i)
+// for (let i=2;i<n;i++)
+//     {    
+//         if( i ===2 || i===3 || i===5 || i==7)
+//             {
+//                 console.log(i)
               
-            }
-       else if(i % 2 === 0 || i % 3===0 ||  i%5 ===0 || i%7===0)
-         {
-            console.log(`${i} , is not prime number`);
+//             }
+//        else if(i % 2 === 0 || i % 3===0 ||  i%5 ===0 || i%7===0)
+//          {
+//             console.log(`${i} , is not prime number`);
             
-        }
+//         }
 
-        else
-        {
-            console.log(`${i} , is prime number`);
+//         else
+//         {
+//             console.log(`${i} , is prime number`);
             
-        }
-    }
+//         }
+//     }
 
 
 
-    //**** Part 3: Feeling Loopy***************/
+ //   **** Part 3: Feeling Loopy***************/
+
+
+    const csvString = "ID,Name,Occupation,Age,42,Bruce,Knight,41,57,Bob,Fry ,Cook,19,63,Blaine,Quiz, Master,58,98,Bill,Doctor ,Assistant,26 " 
+
+    const colors=csvString.split(",")
+      
+    for (const color of colors) {
+        console.log(color);
+      }
+
 
